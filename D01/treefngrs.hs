919ffaf51da1410417c09ef3b89c3c43913f0a40
treefngrs <79084432+treefngrs@users.noreply.github.com>
2021-12-02 13:59:43 +0100

    D01 haskell

--- D01/treefngrs.hs
+++ D01/treefngrs.hs
@@ -1,11 +1,8 @@
-sol1 :: [Int] -> Int
-sol1 ls@(_:ls') = foldr (\(a,b) s -> if a<b then s+1 else s) 0 (zip ls ls')
-
-sol2 :: [Int] -> Int
-sol2 ls@(_:(_:(_:ls'))) = foldr (\(a,b) s -> if a<b then s+1 else s) 0 (zip ls ls')
+sol :: [Int] -> Int -> Int
+sol ls n = foldr (\(a,b) s -> if a<b then s+1 else s) 0 $ zip ls $ drop n ls
 
 main = do
     file <- readFile "input.txt"
     let xs = map read (lines file) :: [Int]
-    putStrLn $ "Star 1: " ++ (show $ sol1 xs)
-    putStrLn $ "Star 2: " ++ (show $ sol2 xs)
+    putStrLn $ "Star 1: " ++ (show $ sol xs 1)
+    putStrLn $ "Star 2: " ++ (show $ sol xs 3)
