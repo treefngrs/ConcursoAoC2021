2307fb90140099ebb5619ff6f4b20a033cccbd7c
treefngrs <79084432+treefngrs@users.noreply.github.com>
2021-12-02 14:02:46 +0100

    D01 python

---
+++ D02/treefngrs.py
@@ -0,0 +1,14 @@
+xs = [(cad, int(d)) for cad,d in map(lambda x: x.split(), open("input.txt").readlines())]
+
+hor,dep,dep2=(0,)*3
+for cad, d in xs:
+	if cad == "up":
+		dep-=d
+	elif cad == "down":
+		dep+=d
+	elif cad == "forward":
+		hor+=d
+		dep2+=dep*d
+
+print("Star 1:", hor*dep)
+print("Star 2:", hor*dep2)
