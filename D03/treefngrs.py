cc33794a6a48a55d8f31697402e52cfccc8b9386
treefngrs <79084432+treefngrs@users.noreply.github.com>
2021-12-03 14:01:18 +0100

    D03 python

--- D03/treefngrs.py
+++ D03/treefngrs.py
@@ -1,12 +1,13 @@
 from functools import reduce
 xs = [list(map(int,list(cad))) for cad in open("input.txt").read().splitlines()]
+
 bools = [2*x>len(xs) for x in [sum(a) for a in zip(*xs)]]
-s1=(reduce(lambda a, b: (a<<1) + b, bools))
-s2=(reduce(lambda a, b: (a<<1) + b,  [not bool for bool in bools]))
+s1=reduce(lambda a, b: (a<<1) + b, bools)
+s2=reduce(lambda a, b: (a<<1) + b,  [not bool for bool in bools])
 print("Star 1:",s1*s2)
 
-i=0
 ys=xs
+i=0
 while(len(xs)>1):
 	s = 2*sum(x[i] for x in xs)>=len(xs)
 	xs = list(filter(lambda a:a[i]==s,xs))
@@ -18,6 +19,6 @@ while(len(ys)>1):
 	ys = list(filter(lambda a:a[i]!=s,ys))
 	i+=1
 
-s1=(reduce(lambda a, b: (a<<1) + b, xs[0]))
-s2=(reduce(lambda a, b: (a<<1) + b, ys[0]))
+s1=reduce(lambda a, b: (a<<1) + b, xs[0])
+s2=reduce(lambda a, b: (a<<1) + b, ys[0])
 print("Star 2:",s1*s2)
