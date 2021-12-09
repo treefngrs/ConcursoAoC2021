4c4df6280d1496c382e3b53af024f1992a7896a5
treefngrs <79084432+treefngrs@users.noreply.github.com>
2021-12-09 20:38:14 +0100

    D09 python

--- D09/treefngrs.py
+++ D09/treefngrs.py
@@ -1,26 +1,20 @@
-xs = [[[int(c),True] for c in line.strip()] for line in open("input.txt")]
+xs = [[int(c) for c in line.strip()] for line in open("input.txt")]
+n,m = len(xs),len(xs[0])
 
-basins,lens=[],[]
-s=0
+def adj(i,j):
+	return [(i+x2,j+y2) for (x2,y2) in [(1,0),(-1,0),(0,1),(0,-1)]]
 
-for i in range(100):
-	for j in range(100):
-		for x,y in [(i+x2,j+y2) for (x2,y2) in [(1,0),(-1,0),(0,1),(0,-1)]]:
-			if x>=0 and x<100 and y>=0 and y<100 and xs[x][y][0]<=xs[i][j][0]:
-				xs[i][j][1] = False
-				break
-		if xs[i][j][1]:
-			s+=xs[i][j][0]+1
-			basins.append([(i,j)])
+def checkadj(num,x,y):
+	return not 0<=x<n or not 0<=y<m or xs[x][y]>num
 
+def checkbas(x,y,ls):
+	return 0<=x<n and 0<=y<m and xs[x][y]!=9 and (x,y) not in ls
+
+basins=[[(i,j)] for i in range(n) for j in range(m) if all([checkadj(xs[i][j],x,y) for x,y in adj(i,j)])]
 for basin in basins:
 	for (i,j) in basin:
-		for x,y in [(i+x2,j+y2) for (x2,y2) in [(1,0),(-1,0),(0,1),(0,-1)]]:
-			if x>=0 and x<100 and y>=0 and y<100 and (x,y) not in basin and xs[x][y][0] != 9:
-				basin.append((x,y))
-	lens.append(len(basin))
+		basin.extend([(x,y) for x,y in adj(i,j) if checkbas(x,y,basin)])
+lens = sorted(list(map(len,basins)), reverse=True)
 
-lens = sorted(lens, reverse=True)
-
-print("Star 1:", s)
+print("Star 1:", sum([1+xs[i][j] for i,j in x][0] for x in basins))
 print("Star 2:", lens[0]*lens[1]*lens[2])
